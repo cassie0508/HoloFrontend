edb1de53ffc6535be07fb081de5ad3e3a80303d3
cassie0508 <chenguanyu1958@gmail.com>
2024-08-26 01:03:41 +0200

    fix bugs and create a workable demo

---
+++ Assets/KinectController_WithDuplicatedReality.cs
@@ -0,0 +1,21 @@
+using PubSub;
+using UnityEngine;
+
+public class KinectController_WithDuplicatedReality : KinectSubscriberCopy
+{
+
+    [Header("Duplicated Reality")]
+    public bool EnableDuplication = true;
+    public Transform RegionOfInterest;
+    public Transform DuplicatedReality;
+
+    public override void OnSetPointcloudProperties(Material pointcloudMat)
+    {
+        if(EnableDuplication) pointcloudMat.EnableKeyword("_DUPLICATE_ON");
+        else pointcloudMat.DisableKeyword("_DUPLICATE_ON");
+
+        pointcloudMat.SetMatrix("_Roi2Dupl", DuplicatedReality.localToWorldMatrix * RegionOfInterest.worldToLocalMatrix);
+        pointcloudMat.SetMatrix("_ROI_Inversed", RegionOfInterest.worldToLocalMatrix);
+        pointcloudMat.SetMatrix("_Dupl_Inversed", DuplicatedReality.worldToLocalMatrix);
+    }
+}
--- Assets/KinectSubscriberCopy.cs
+++ Assets/KinectSubscriberCopy.cs
@@ -1,5 +1,7 @@
 using NetMQ;
 using System;
+using System.Net.Sockets;
+using System.Net;
 using UnityEngine;
 using UnityMainThreadDispatcher;
 
@@ -13,11 +15,6 @@ namespace PubSub
         public Shader OcclusionShader;
         [Range(0.01f, 0.1f)]
         public float MaxPointDistance = 0.02f;
-
-        [Header("Duplicated Reality")]
-        public bool EnableDuplication = true;
-        public Transform RegionOfInterest;
-        public Transform DuplicatedReality;
 
         [Header("Background Configs\n(Only works if this script is attached onto the camera)")]
         public bool EnableARBackground = true;
@@ -34,35 +31,139 @@ namespace PubSub
         [SerializeField] private int DepthHeight;
         [SerializeField] private int IRWidth;
         [SerializeField] private int IRHeight;
-        [SerializeField] private Texture2D xylookup;
-        [SerializeField] private Matrix4x4 color2DepthCalibration;
+        [SerializeField] private Texture2D XYLookup;
+        [SerializeField] private Matrix4x4 Color2DepthCalibration;
         [SerializeField] private Material PointcloudMat;
         [SerializeField] private Material OcclusionMat;
 
         private Subscriber subscriber;
 
-        [SerializeField] private string host = "10.32.211.162";
+        [SerializeField] private string host;
         [SerializeField] private string port = "12345";
 
+        public virtual void OnSetPointcloudProperties(Material pointcloudMat) { }
 
         void Start()
         {
             Debug.Log("Starting subscriber...");
+            host = GetLocalIPAddress();
             subscriber = new Subscriber(host, port);
-            subscriber.AddTopicCallback("XYTable", data => OnXYTableReceived(data));
-            subscriber.AddTopicCallback("Color2DepthCalibration", data => OnColor2DepthCalibrationReceived(data));
-            subscriber.AddTopicCallback("Capture", data => OnCaptureReceived(data));
-            subscriber.AddTopicCallback("AllFrames", data => OnAllFramesReceived(data));
+            subscriber.AddTopicCallback("Camera", data => OnCameraReceived(data));
+            subscriber.AddTopicCallback("Frame", data => OnFrameReceived(data));
             Debug.Log("Subscriber setup complete.");
         }
 
-        private void OnColor2DepthCalibrationReceived(byte[] data)
+        public string GetLocalIPAddress()
+        {
+            var host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (var ip in host.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    return ip.ToString();
+                }
+            }
+            throw new Exception("No network adapters with an IPv4 address in the system!");
+        }
+
+        private void OnCameraReceived(byte[] data)
         {
             UnityMainThreadDispatcher.Dispatcher.Enqueue(() =>
             {
-                color2DepthCalibration = ByteArrayToMatrix4x4(data);
-                Debug.Log("Received and applied Color2DepthCalibration.");
+                // Parse camera data
+                int xyLookupDataLength = BitConverter.ToInt32(data, 0);
+                int calibrationDataLength = BitConverter.ToInt32(data, sizeof(int) * 1);
+                int cameraSizeDataLength = BitConverter.ToInt32(data, sizeof(int) * 2);
+
+                byte[] xyLookupData = new byte[xyLookupDataLength];
+                Buffer.BlockCopy(data, sizeof(int) * 3, xyLookupData, 0, xyLookupDataLength);
+                byte[] calibrationData = new byte[calibrationDataLength];
+                Buffer.BlockCopy(data, sizeof(int) * 3 + xyLookupDataLength, calibrationData, 0, calibrationDataLength);
+                byte[] cameraSizeData = new byte[cameraSizeDataLength];
+                Buffer.BlockCopy(data, sizeof(int) * 3 + xyLookupDataLength + calibrationDataLength, cameraSizeData, 0, cameraSizeDataLength);
+
+                // Setup texture
+                int[] captureArray = new int[6];
+                Buffer.BlockCopy(cameraSizeData, 0, captureArray, 0, cameraSizeData.Length);
+                ColorWidth = captureArray[0];
+                ColorHeight = captureArray[1];
+                DepthWidth = captureArray[2];
+                DepthHeight = captureArray[3];
+                IRWidth = captureArray[4];
+                IRHeight = captureArray[5];
+
+                SetupTextures(ref ColorImage, ref DepthImage, ref ColorInDepthImage);   // TODO: ref or not?
+
+                // Setup point cloud shader
+                if(XYLookup == null)
+                {
+                    XYLookup = new Texture2D(DepthImage.width, DepthImage.height, TextureFormat.RGBAFloat, false);
+                    XYLookup.LoadRawTextureData(xyLookupData);
+                    XYLookup.Apply();
+                }
+
+                Color2DepthCalibration = ByteArrayToMatrix4x4(calibrationData);
+
+                PointcloudMat = SetupPointcloudShader(PointCloudShader, ColorInDepthImage, ref DepthImage);
+                OcclusionMat = SetupPointcloudShader(OcclusionShader, ColorInDepthImage, ref DepthImage);
             });
+        }
+
+        private void OnFrameReceived(byte[] data)
+        {
+            UnityMainThreadDispatcher.Dispatcher.Enqueue(() => {
+                // Parse frame data
+                int colorDataLength = BitConverter.ToInt32(data, 0);
+                int depthDataLength = BitConverter.ToInt32(data, sizeof(int));
+                int colorInDepthDataLength = BitConverter.ToInt32(data, sizeof(int) * 2);
+
+                byte[] colorData = new byte[colorDataLength];
+                Buffer.BlockCopy(data, sizeof(int) * 3, colorData, 0, colorDataLength);
+                byte[] depthData = new byte[depthDataLength];
+                Buffer.BlockCopy(data, sizeof(int) * 3 + colorDataLength, depthData, 0, depthDataLength);
+                byte[] colorInDepthData = new byte[colorInDepthDataLength];
+                Buffer.BlockCopy(data, sizeof(int) * 3 + colorDataLength + depthDataLength, colorInDepthData, 0, colorInDepthDataLength);
+
+                // Apply data to textures
+                ColorImage.LoadRawTextureData(colorData);
+                ColorImage.Apply();
+                DepthImage.LoadRawTextureData(depthData);
+                DepthImage.Apply();
+                ColorInDepthImage.LoadRawTextureData(colorInDepthData);
+                ColorInDepthImage.Apply();
+
+                // Set point cloud shader
+                int pixel_count = DepthImage.width * DepthImage.height;
+                PointcloudMat.SetMatrix("_PointcloudOrigin", transform.localToWorldMatrix);
+                PointcloudMat.SetFloat("_MaxPointDistance", MaxPointDistance);
+
+                OnSetPointcloudProperties(PointcloudMat);
+
+                if (!UseOcclusionShader)
+                {
+                    PointcloudMat.EnableKeyword("_ORIGINALPC_ON");
+                }
+                else
+                {
+                    PointcloudMat.DisableKeyword("_ORIGINALPC_ON");
+
+                    OcclusionMat.SetMatrix("_PointcloudOrigin", transform.localToWorldMatrix);
+                    OcclusionMat.SetFloat("_MaxPointDistance", MaxPointDistance);
+                    Graphics.DrawProcedural(OcclusionMat, new Bounds(transform.position, Vector3.one * 10), MeshTopology.Points, pixel_count);
+                }
+
+                Graphics.DrawProcedural(PointcloudMat, new Bounds(transform.position, Vector3.one * 10), MeshTopology.Points, pixel_count);
+            });
+        }
+
+        private void SetupTextures(ref Texture2D Color, ref Texture2D Depth, ref Texture2D ColorInDepth)
+        {
+            if (Color == null)
+                Color = new Texture2D(ColorWidth, ColorHeight, TextureFormat.BGRA32, false);
+            if (Depth == null)
+                Depth = new Texture2D(DepthWidth, DepthHeight, TextureFormat.R16, false);
+            if (ColorInDepth == null)
+                ColorInDepth = new Texture2D(IRWidth, IRHeight, TextureFormat.BGRA32, false);
         }
 
         private Matrix4x4 ByteArrayToMatrix4x4(byte[] byteArray)
@@ -79,111 +180,6 @@ namespace PubSub
             return matrix;
         }
 
-        private void OnXYTableReceived(byte[] data)
-        {
-            UnityMainThreadDispatcher.Dispatcher.Enqueue(() =>
-            {
-                if (xylookup == null || xylookup.width * xylookup.height != data.Length / sizeof(float) / 4)
-                {
-                    //xylookup = new Texture2D(DepthImage.width, DepthImage.height, TextureFormat.RGBAFloat, false);
-                    xylookup = new Texture2D(640, 576, TextureFormat.RGBAFloat, false);
-                }
-
-                xylookup.LoadRawTextureData(data);
-                xylookup.Apply();
-
-                Debug.Log("Received and applied XYTable data.");
-            });
-        }
-
-
-        private void OnAllFramesReceived(byte[] data)
-        {
-            UnityMainThreadDispatcher.Dispatcher.Enqueue(() => {
-                int colorDataLength = BitConverter.ToInt32(data, 0);
-                int depthDataLength = BitConverter.ToInt32(data, sizeof(int));
-                int colorInDepthDataLength = BitConverter.ToInt32(data, sizeof(int) * 2);
-
-                byte[] colorData = new byte[colorDataLength];
-                byte[] depthData = new byte[depthDataLength];
-                byte[] colorInDepthData = new byte[colorInDepthDataLength];
-
-                Buffer.BlockCopy(data, sizeof(int) * 3, colorData, 0, colorDataLength);
-                Buffer.BlockCopy(data, sizeof(int) * 3 + colorDataLength, depthData, 0, depthDataLength);
-                Buffer.BlockCopy(data, sizeof(int) * 3 + colorDataLength + depthDataLength, colorInDepthData, 0, colorInDepthDataLength);
-
-                ColorImage.LoadRawTextureData(colorData);
-                ColorImage.Apply();
-
-                DepthImage.LoadRawTextureData(depthData);
-                DepthImage.Apply();
-
-                ColorInDepthImage.LoadRawTextureData(colorInDepthData);
-                ColorInDepthImage.Apply();
-
-                int pixel_count = DepthImage.width * DepthImage.height;
-                PointcloudMat.SetMatrix("_PointcloudOrigin", transform.localToWorldMatrix);
-                PointcloudMat.SetFloat("_MaxPointDistance", MaxPointDistance);
-
-                if (EnableDuplication) PointcloudMat.EnableKeyword("_DUPLICATE_ON");
-                else PointcloudMat.DisableKeyword("_DUPLICATE_ON");
-
-                PointcloudMat.SetMatrix("_Roi2Dupl", DuplicatedReality.localToWorldMatrix * RegionOfInterest.worldToLocalMatrix);
-                PointcloudMat.SetMatrix("_ROI_Inversed", RegionOfInterest.worldToLocalMatrix);
-                PointcloudMat.SetMatrix("_Dupl_Inversed", DuplicatedReality.worldToLocalMatrix);
-
-                if (!UseOcclusionShader)
-                {
-                    PointcloudMat.EnableKeyword("_ORIGINALPC_ON");
-                }
-                else
-                {
-                    PointcloudMat.DisableKeyword("_ORIGINALPC_ON");
-
-                    OcclusionMat.SetMatrix("_PointcloudOrigin", transform.localToWorldMatrix);
-                    OcclusionMat.SetFloat("_MaxPointDistance", MaxPointDistance);
-                    Graphics.DrawProcedural(OcclusionMat, new Bounds(transform.position, Vector3.one * 10), MeshTopology.Points, pixel_count);
-                }
-
-                Graphics.DrawProcedural(PointcloudMat, new Bounds(transform.position, Vector3.one * 10), MeshTopology.Points, pixel_count);
-
-            });
-        }
-
-        private void OnCaptureReceived(byte[] data)
-        {
-            UnityMainThreadDispatcher.Dispatcher.Enqueue(() => {
-                int[] capture = new int[6];
-                Buffer.BlockCopy(data, 0, capture, 0, data.Length);
-
-                ColorWidth = capture[0];
-                ColorHeight = capture[1];
-                DepthWidth = capture[2];
-                DepthHeight = capture[3];
-                IRWidth = capture[4];
-                IRHeight = capture[5];
-
-                Debug.Log(capture);
-
-                Debug.Log($"Received Capture Dimensions: ColorWidth={ColorWidth}, ColorHeight={ColorHeight}, DepthWidth={DepthWidth}, DepthHeight={DepthHeight}, IRWidth={IRWidth}, IRHeight={IRHeight}");
-
-                SetupTextures(ref ColorImage, ref DepthImage, ref ColorInDepthImage);
-
-                PointcloudMat = SetupPointcloudShader(PointCloudShader, ColorInDepthImage, ref DepthImage);
-                OcclusionMat = SetupPointcloudShader(OcclusionShader, ColorInDepthImage, ref DepthImage);
-            });
-        }
-
-        private void SetupTextures(ref Texture2D Color, ref Texture2D Depth, ref Texture2D ColorInDepth)
-        {
-            if (Color == null)
-                Color = new Texture2D(ColorWidth, ColorHeight, TextureFormat.BGRA32, false);
-            if (Depth == null)
-                Depth = new Texture2D(DepthWidth, DepthHeight, TextureFormat.R16, false);
-            if (ColorInDepth == null)
-                ColorInDepth = new Texture2D(IRWidth, IRHeight, TextureFormat.BGRA32, false);
-        }
-
         private Material SetupPointcloudShader(Shader shader, Texture2D ColorInDepth, ref Texture2D Depth)
         {
             var PointCloudMat = new Material(shader);
@@ -198,10 +194,8 @@ namespace PubSub
             PointCloudMat.SetInt("_DepthWidth", Depth.width);
             PointCloudMat.SetInt("_DepthHeight", Depth.height);
 
-            PointCloudMat.SetTexture("_XYLookup", xylookup);
-
-            
-            PointCloudMat.SetMatrix("_Col2DepCalibration", color2DepthCalibration);
+            PointCloudMat.SetTexture("_XYLookup", XYLookup);
+            PointCloudMat.SetMatrix("_Col2DepCalibration", Color2DepthCalibration);
 
             return PointCloudMat;
         }
@@ -222,11 +216,6 @@ namespace PubSub
         {
             Debug.Log("Destroying subscriber...");
             subscriber.Dispose();
-        }
-
-        private void OnTestMessage(string topic, byte[] data)
-        {
-            Debug.Log($"Received data on topic: {topic}, data length: {data.Length}");
         }
     }
 }
