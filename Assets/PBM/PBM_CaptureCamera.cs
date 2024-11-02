b81aeb38150ddbb5eb6ececda4b1c775d4154894
cassie0508 <chenguanyu1958@gmail.com>
2024-11-02 18:03:33 +0100

    mirror version

--- Assets/PBM/PBM_CaptureCamera.cs
+++ Assets/PBM/PBM_CaptureCamera.cs
@@ -81,6 +81,7 @@ public class PBM_CaptureCamera : MonoBehaviour
         }
 
         _Camera = GetComponent<Camera>();
+        _Camera.stereoTargetEye = StereoTargetEyeMask.None; // Set stereo target to none
         _Camera.cullingMask &= ~(1 << LayerMask.NameToLayer("PBM"));
         _Camera.usePhysicalProperties = true;
         Width = (int)_Camera.sensorSize.x;
@@ -240,54 +241,5 @@ public class PBM_CaptureCamera : MonoBehaviour
 
             Graphics.Blit(source, ViewRenderTexture, RealVirtualMergeMaterial);
         }
-    }
-
-
-    //private void OnRenderImage(RenderTexture source, RenderTexture destination)
-    //{
-    //    if (ColorImage != null && hasFirstFrameReceived)
-    //    {
-    //        Resize(ColorImage, source.width, source.height);
-
-    //        RealVirtualMergeMaterial.mainTexture = source;
-    //        RealVirtualMergeMaterial.SetTexture("_RealContentTex", ColorImage);
-
-    //        Debug.Log($"OnRenderImage: source is {source}, " +
-    //            $"ColorImage size is {ColorImage.width} {ColorImage.height}, " +
-    //            $"ViewRenderTexture size is {ViewRenderTexture.width} {ViewRenderTexture.height}");
-
-    //        Graphics.Blit(source, ViewRenderTexture, RealVirtualMergeMaterial);
-    //    }
-    //}
-
-    // https://github.com/ababilinski/unity-gpu-texture-resize/blob/master/ResizeTool.cs
-    private void Resize(Texture2D texture2D, int targetX, int targetY, bool mipmap = true, FilterMode filter = FilterMode.Bilinear)
-    {
-        //create a temporary RenderTexture with the target size
-        RenderTexture rt = RenderTexture.GetTemporary(targetX, targetY, 0, RenderTextureFormat.ARGB32, RenderTextureReadWrite.Default);
-
-        //set the active RenderTexture to the temporary texture so we can read from it
-        RenderTexture.active = rt;
-
-        //Copy the texture data on the GPU - this is where the magic happens [(;]
-        Graphics.Blit(texture2D, rt);
-        //resize the texture to the target values (this sets the pixel data as undefined)
-        texture2D.Resize(targetX, targetY, texture2D.format, mipmap);
-        texture2D.filterMode = filter;
-
-        try
-        {
-            //reads the pixel values from the temporary RenderTexture onto the resized texture
-            texture2D.ReadPixels(new Rect(0.0f, 0.0f, targetX, targetY), 0, 0);
-            //actually upload the changed pixels to the graphics card
-            texture2D.Apply();
-        }
-        catch
-        {
-            Debug.LogError("Read/Write is not enabled on texture " + texture2D.name);
-        }
-
-
-        RenderTexture.ReleaseTemporary(rt);
     }
 }
